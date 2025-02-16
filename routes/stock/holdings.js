c739f5b9f42406367103e4f9bf1449447e9afa01
Benny <benny88811@gmail.com>
2025-02-16 21:08:47 +0800

    error catch

--- routes/stock/holdings.js
+++ routes/stock/holdings.js
@@ -36,10 +36,11 @@ const getVolKData = async (symbol) => {
           break;
       }
     }
-    VolKData.totalDifferenceVolK1D = data?.list[0]?.totalDifferenceVolK;
-    VolKData.totalOverbuyVolK = data?.list[0]?.totalOverbuyVolK;
-    VolKData.totalOversellVolK = data?.list[0]?.totalOversellVolK;
-    VolKData.tradeVolumeRate = data?.list[0]?.tradeVolumeRate;
+    VolKData.totalDifferenceVolK1D =
+      data?.list[0]?.totalDifferenceVolK || 'failed';
+    VolKData.totalOverbuyVolK = data?.list[0]?.totalOverbuyVolK || 'failed';
+    VolKData.totalOversellVolK = data?.list[0]?.totalOversellVolK || 'failed';
+    VolKData.tradeVolumeRate = data?.list[0]?.tradeVolumeRate || 'failed';
 
     return new Promise((resolve) => {
       resolve(VolKData);
@@ -49,10 +50,13 @@ const getVolKData = async (symbol) => {
     return new Promise((resolve) => {
       resolve({
         symbol,
-        totalDifferenceVolK: 'failed',
         totalOverbuyVolK: 'failed',
         totalOversellVolK: 'failed',
         tradeVolumeRate: 'failed',
+        totalDifferenceVolK1D: 'failed',
+        totalDifferenceVolK5D: 'failed',
+        totalDifferenceVolK10D: 'failed',
+        totalDifferenceVolK20D: 'failed',
       });
     });
   }
